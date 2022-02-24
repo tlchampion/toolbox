e3e1fc2ccbcf82d56419276ee2fbbab27f8b9c25
Thomas Champion <thomas@thomaschampion.net>
2022-02-23 18:49:59 -0800

    added load_and_prep_image

--- toolbox/helper_functions.py
+++ toolbox/helper_functions.py
@@ -8,6 +8,32 @@ import itertools
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import confusion_matrix
+import tensorflow as tf
+from sklearn.pipeline import Pipeline
+
+
+def load_and_prep_image(filename, img_shape=224, scale=True):
+    """
+    Reads in an image from filename, turns it into a tensor and reshapes into
+    (224, 224, 3).
+
+    Parameters
+    ----------
+    filename (str): string filename of target image
+    img_shape (int): size to resize target image to, default 224
+    scale (bool): whether to scale pixel values to range(0, 1), default True
+    """
+    # Read in the image
+    img = tf.io.read_file(filename)
+    # Decode it into a tensor
+    img = tf.image.decode_jpeg(img)
+    # Resize the image
+    img = tf.image.resize(img, [img_shape, img_shape])
+    if scale:
+        # Rescale the image (get all values between 0 and 1)
+        return img/255.
+    else:
+        return img
 
 
 def get_column_names_from_ColumnTransformer(column_transformer):
