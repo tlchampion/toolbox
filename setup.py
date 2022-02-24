be4c29b07b321dc48f95511ef28a16522bf2ffab
Thomas Champion <thomas@thomaschampion.net>
2022-02-23 18:18:37 -0800

    updated directory structure

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(
     author='Thomas Champion',
     author_email="thomas@thomaschampion.net",
     description='Collection of useful scripts for use in ML analysis/modeling',
+    long_description="A collection of useful scripts for use in ML analysis/modeling",
     long_description_content_type="text/markdown",
     url='https://github.com/tlchampion/toolbox',
     project_urls={
